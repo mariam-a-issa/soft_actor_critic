ba4ab4a817f3e160bc0b0c0f21a8382d29c061d8
Ian Bryant <iwbryant@uci.edu>
2024-03-24 02:09:59 -0700

    Updated to use batches. Needs testing to see if implemented correctly

--- nn_sac.py
+++ nn_sac.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch import Tensor
 import torch.optim as optim
 from torch.distributions import MultivariateNormal
+from torch.nn import MSELoss
 from collections import namedtuple
 from copy import deepcopy
 import gym
@@ -18,8 +19,6 @@ GAMMA = .99
 
 Transition = namedtuple('Transition', ['state', 'action', 'next_state', 'next_action', 'reward']) #If bad performance just switch to a tensor
 
-
-#TODO How to make it a batch for the updates?
 
 
 class BaseNN(nn.Module):
@@ -39,6 +38,7 @@ class BaseNN(nn.Module):
                                 nn.Linear(self._hidden_size, self.output_size))
 
     def forward(self, x : Tensor) -> Tensor:
+        """Using batchs x should be N x D where N is the number of batches"""
         return self.layers(x)
     
 
@@ -49,21 +49,21 @@ class ValueFunction(BaseNN):
         super().__init__(*args)
         self._optim = optim.Adam(self.parameters(), STEP_V)
         self._actor = actor
-        self._q_function = q_function
+        self._q_func = q_function
 
         self._num_updates = 1 #For logging
 
     def update_parameters(self, trans : Transition) -> None:
-        action, log_prob = self._actor(trans.state)
-        error = float(self(trans.state) - self._q_function(torch.cat((trans.state, action), dim = 0)) + log_prob)
-        
-        val = self(trans.state) * error
-        self._optim.zero_grad()
-        val.backward()
+        actions, log_prob = self._actor(trans.state)
+        input_tensor = torch.cat((trans.state, actions), dim = 1)
+        error : Tensor = 1/2 * ((self(trans.action) - (self._q_func(input_tensor) - log_prob)) ** 2)
+        ex_error = error.mean()
 
+        self._optim.zero_grad()
+        ex_error.backward()
         self._optim.step()
 
-        writer.add_scalar('Value error', error, self._num_updates)
+        writer.add_scalar('Value error', ex_error, self._num_updates)
         self._num_updates += 1
 
 
@@ -71,11 +71,16 @@ class ValueFunction(BaseNN):
 class TargetValueFunction:
     """Target target value function that contains method to update parameters not using gradient according to pseudocode""" #Is the pseudocode representation the exponentially moving average?
 
+    def __init__(self) -> None:
+        self._v_tar = None
+        self._v_func = None
+
     def upload_v_func(self, v_func : ValueFunction) -> None:
         self._v_tar = deepcopy(v_func)
         self._v_func = v_func
 
     def update_parameters(self, trans : Transition) -> None:
+        """Update parameters according to algorithim 1"""
         with torch.no_grad():
             for t_param, v_param in zip(self._v_tar.parameters(), self._v_func.parameters()):
                 new_value = TAU * v_param + (1 - TAU) * t_param
@@ -103,15 +108,16 @@ class QModel(BaseNN):
         self._num_updates = 1
     
     def update_parameters(self, trans : Transition) -> None:
-        input_tensor = torch.cat((trans.state, trans.action), dim = 0)
-        error = float(self(input_tensor)) - trans.reward - GAMMA * self._v_func(trans.next_state)
+        """Update parameters according to equations 7 and 8"""
+        input_tensor = torch.cat((trans.state, trans.action), dim = 1)
+        error : Tensor = 1/2 * ((self(input_tensor) - (trans.reward + self._v_func(trans.next_state))) ** 2)
+        ex_error = error.mean()
 
-        val = self(input_tensor) * error
-        self._optim.zero_grad()
-        val.backward()
+        self._optim.zero_grad
+        ex_error.backward()
         self._optim.step()
 
-        writer.add_scalar(f'Q function {self._network_id} error', error, self._num_updates)
+        writer.add_scalar(f'Q function {self._network_id} error', ex_error, self._num_updates)
         self._num_updates += 1
 
 
@@ -129,6 +135,7 @@ class QFunction:
         return torch.min(self._q1(x), self._q2(x))
 
     def update_parameters(self, trans : Transition) -> None:
+        """Will update both q models of the q functions"""
         for q in self._list_q_funcs():
             q.update_parameters(trans)
 
@@ -140,83 +147,78 @@ class Actor(BaseNN):
     """Actor updated according to equation 13"""
 
     def __init__(self, input_size : int, output_size : int, *, q_function : QFunction) -> None:
-        super().__init__(input_size, output_size * 2) #Needs to be twice as we need to get mean and covar vectors
+        super().__init__(input_size, output_size)
         self._q_function = q_function
-        self.real_output_size = output_size
 
-        #May need to pick the random numbers differently
-        mean = torch.randn(self.real_output_size) 
-        variance = float(torch.rand(1)) 
-
-        covar = variance * torch.eye(self.real_output_size)
-        self._s_dist = MultivariateNormal(mean, covar)
+        #Split the last layer by removing it and having two seperate output layers
+        #Allows batch training
+        self.layers = self.layers[:-1]
+        self._mean_lin = nn.Linear(self._hidden_size, output_size)
+        self._covar_lin = nn.Linear(self._hidden_size, output_size)
+        self._optim = optim.Adam(self.layers.parameters() + self._mean_lin() + self._covar_lin.parameters(),
+                                 STEP_ACTOR)
     
-    def forward(self, x : Tensor) -> tuple[Tensor, Tensor]:
-        
-        mean, covar = self._forward_gaussian(x)
-
-        dist = MultivariateNormal(mean, covar)
-
+    def forward(self, x : Tensor) -> Tensor:
+        """Will reuturn a tensor that represents the action for a single or batch of a state"""
+        dist = self._dist(x)
         action = dist.sample()
-        action.requires_grad = True
+        return action, dist.log_prob(action)
 
-        log_prob = dist.log_prob(action)
-
-        return action, log_prob
     
     def update_parameters(self, trans : Transition) -> None:
-        pass
-        """
-        For gradients that are w.r.t the parameters:
-        Do output.backward() get grad of each parameter and add to a tensor
+        """Updates the parameters using equation 12"""
 
-        For gradients that are w.r.t action:
-        Do action.requires_grad_() then find the grad of the action after ouput.backward()
+        dist = self._dist(trans.state)
+        actions = dist.rsample()
+        log_probs = dist.log_prob(actions)
+        loss : Tensor = log_probs - self._q_function(torch.cat((trans.state, actions), dim=1)) #Concate in dimension where the vectors representing state and action are stored
+        
+        self._optim.zero_grad()
+        loss.mean().backward()
+        self._optim.step()
 
-        How will we multiple a vector of with dimension size of actor and of size parameters?
-        """
+    def _dist(self, x : Tensor) -> MultivariateNormal:
+        """Will create a distribution for either a single or batch of a state """
 
-        other_a = deepcopy(self)
+        mean = self._mean_lin(self.layers(x))
+        covar = self._covar_lin(self.layers(x))
+        covar = covar.pow(2)
 
-        mean, covar = other_a._forward_gaussian(trans.state)
-        noisy_v = other_a._noisy_vector()
+        if len(covar.shape) == 2: #When dealing with batches
+            covar_m = torch.diag(covar[0]).unsqueeze(dim = 0)
+            for cv in covar[1:]:
+                covar_m = torch.cat((covar_m, torch.diag(cv).unsqueeze(dim = 0)), dim = 0)
+        else:
+            covar_m = torch.diag(covar)
 
-        action = noisy_v @ covar + mean
-        o = optim.Adam(other_a.parameters())
-
-        jacobian = Tensor([])
-
-        for element in action:
-            o.zero_grad()
-            element.backward(retain_graph=True)
-
-            param_grad = Tensor([])
-            for param in other_a.parameters():
-                param_grad = torch.cat((param_grad, param.grad.flatten(start_dim=0)), dim = 0)
-            
-            jacobian = torch.cat((jacobian, param_grad.unsqueeze(dim=0)), dim = 0)
-
-
-
-
-
+        return MultivariateNormal(mean, covar_m)
+    
     
     def _forward_gaussian(self, x : Tensor) -> tuple[Tensor, Tensor]:
         """Will calculate the mean vector and the covar matrix"""
+
+    def _forward_gaussian(self, x : Tensor) -> tuple[Tensor, Tensor]:
+        """Will calculate the mean vector and the covar matrix"""
+
 
         output = self.layers(x)
         mean = output[:self.real_output_size]
         std = output[self.real_output_size:]
         covar = std.pow(2)
         covar = torch.diag(covar)
-
-        return mean, covar
+    
+        output = self.layers(x)
+        mean = output[:self.real_output_size]
+        std = output[self.real_output_size:]
+        covar = std.pow(2)
+        covar = torch.diag(covar)
     
     #TODO make update parameters and sample from spherical guassian
-
-    def _noisy_vector(self) -> Tensor:
-        """Will create a vector epsilon sampled from a spherical guassian"""
         
+    #TODO make update parameters and sample from spherical guassian
+        
+        return self._s_dist.sample()
+
         return self._s_dist.sample()
 
 
@@ -242,14 +244,14 @@ def train(gm : gym.Env, len_state : int , len_output : int, * , max_game : int =
     #list_networks = [v_func, q_func, actor, target_v] This list should be used if training actor
 
     num_games = 0
+    episodes = 0
 
     state = gm.reset()
     state = Tensor(state[0])
-    action, _ = actor(state) #can use random action torch.FloatTensor(1).uniform_(-2.0, 2.0)
+    action, _ = actor.action_log(state) #can use random action torch.FloatTensor(1).uniform_(-2.0, 2.0)
     
     try:
-        while max_game == None or max_game > num_games:
-            print(action)
+        while max_game is None or max_game > num_games:
             next_state, reward, done, _, _ = gm.step(action.detach().numpy())
             next_state = Tensor(next_state)
             next_action, _ = actor(next_state)
@@ -259,17 +261,20 @@ def train(gm : gym.Env, len_state : int , len_output : int, * , max_game : int =
             for net in list_networks:
                 net.update_parameters(trans)
 
-            if done:
+            episodes += 1
+
+            if done or episodes > 500:
                 state = gm.reset()
                 action, _ = actor(state) #can use random action torch.FloatTensor(1).uniform_(-2.0, 2.0)
                 num_games += 1
-
+                episodes = 0
+            
             state = next_state
             action = next_action
     finally:
         gm.close()
 
 if __name__ == '__main__':
-    env = gym.make('Pendulum-v1', render_mode='human')
-    train(env, 3, 1)
+    env = gym.make('BipedalWalker-v3', render_mode = 'human')
+    train(env, 24, 4)
     
