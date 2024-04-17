4c08ed522360565adea8ca8511b9ecb50c2fd36d
Ian Bryant <iwbryant@uci.edu>
2024-04-17 14:12:33 -0700

    Update train_script

--- train_script.py
+++ train_script.py
@@ -3,6 +3,11 @@ from train_loop import train
 def train_script():
     """Will do multiple training loops"""
 
+    actor_lr = [.01, .005, .001, .005, .0001, .00005, .00001]
+
+    for value in actor_lr:
+        train(f"policy_lr'{value}'", policy_lr=value, log_dir='runs/policy_lr_experiment')
+
     critic_lr = [.01, .005, .001, .0005, .0001, .00005, .00001]
 
     for value in critic_lr:
