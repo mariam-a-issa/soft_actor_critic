c82f6b2306762f1b84ade0c232ef94a834db8c7c
Ian Bryant <iwbryant@uci.edu>
2025-07-08 23:59:27 -0700

    Added name of experiment

--- train_script.py
+++ train_script.py
@@ -9,7 +9,7 @@ from utils import Config
 from training_pipeline import train
 
 PROJECT_NAME = 'Bundle Feature Encoder'
-MAIN_EXPERIMENT_NAME = 'non-linear-encoder-cleaned'
+MAIN_EXPERIMENT_NAME = 'non-linear-encoder-bind-focus'
 NUM_RUNS = 1
 OTHER_HPARAMS = { #Just the default params that may be different than the ones in the training file
     'wandb_project_name' : PROJECT_NAME,
