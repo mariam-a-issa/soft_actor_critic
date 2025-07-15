e0b96964a6fe0a68fb9763b31ee2ebf14ad17d3f
Ian Bryant <iwbryant@uci.edu>
2025-07-14 19:35:54 -0700

    Copy Paste of what was in the old github commit

--- train_script.py
+++ train_script.py
@@ -9,7 +9,7 @@ from utils import Config
 from training_pipeline import train
 
 PROJECT_NAME = 'Bundle Feature Encoder'
-MAIN_EXPERIMENT_NAME = 'non-linear-assume-no-bug'
+MAIN_EXPERIMENT_NAME = 'non-linear-copy-paste'
 NUM_RUNS = 1
 OTHER_HPARAMS = { #Just the default params that may be different than the ones in the training file
     'wandb_project_name' : PROJECT_NAME,
