1fc8c38a4fb09c5cd1ee8c7215281a68adc21e67
Ian Bryant <iwbryant@uci.edu>
2025-08-12 01:27:30 -0700

    Only normalize at the end

--- implementation/mil_hdc/encoders/gbubi_encoder.py
+++ implementation/mil_hdc/encoders/gbubi_encoder.py
@@ -69,12 +69,12 @@ class GBUBIEncoder:
         #Setup Tensors
         is_subnet = nodes.x[:, 0] == 1
         node_features = 2 * nodes.x[~is_subnet].float() - 1 # n x f. Number of nodes x number of features. Do 2*x - 1 for hamming distance
-        encoded_features = F.normalize(node_features[:, 1:] @ self._base, p=2, dim = 1) # n x d
-        adj_matrix = to_dense_adj(nodes.edge_index).squeeze()
-        subnet_node_adj_matrix = adj_matrix[is_subnet][:, ~is_subnet] # sub_nets x n
+        encoded_features = node_features[:, 1:] @ self._base, p=2, dim = 1 # n x d
 
         #Build Subnets
-        encoded_subnet = F.normalize(subnet_node_adj_matrix @ encoded_features, p=2, dim = 1) #sub_nets x d
+        adj_matrix = to_dense_adj(nodes.edge_index).squeeze()
+        subnet_node_adj_matrix = adj_matrix[is_subnet][:, ~is_subnet] # sub_nets x n
+        encoded_subnet = subnet_node_adj_matrix @ encoded_features, p=2, dim = 1 #sub_nets x d
         perm_encoded_subnet = permute_rows_by_shifts(encoded_subnet, torch.ones(encoded_subnet.shape[0], dtype=torch.int))
         
         #Build Subnet Connections
@@ -84,16 +84,15 @@ class GBUBIEncoder:
         subnet_batch_idx = nodes.batch[is_subnet]
         num_graphs = int(subnet_batch_idx.max()) + 1
         bundled = scatter_add(binded_subnets, subnet_batch_idx, dim=0, dim_size=num_graphs)  # graph x d
-        state_per_graph = F.normalize(bundled, p=2, dim=1)
 
         #Embedded devices
-        expanded_graphs = state_per_graph[nodes.batch[~is_subnet]]
+        expanded_graphs = bundled[nodes.batch[~is_subnet]]
         embed_devices = encoded_features * permute_rows_by_shifts(expanded_graphs, torch.ones(expanded_graphs.shape[0], dtype=torch.int))
 
         #RBF Kernal Activation
         #activated_devices = torch.exp(1j * (embed_devices @ self._rbf_base + self._bias))
 
-        return embed_devices, nodes.batch[~is_subnet]
+        return F.normalize(embed_devices), nodes.batch[~is_subnet]
 
     def _bind_subnets_hadamard(self, 
         encoded_subnet: torch.Tensor,        # [N, D]
@@ -124,7 +123,7 @@ class GBUBIEncoder:
             deg = A.sum(dim=1, keepdim=True).clamp_min(1).to(out.dtype)
             out = out / deg
 
-        return out
+        return F.normalize(out, p = 2, dim = 1)
 
 
     def to(self, device : torch.device) -> None:
