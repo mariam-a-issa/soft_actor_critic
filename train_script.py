20f46480887c8e100a682e895c2e7c1f90f2216c
Ian Bryant <iwbryant@uci.edu>
2025-08-15 20:40:18 -0700

    name

--- train_script.py
+++ train_script.py
@@ -9,7 +9,7 @@ from utils import Config
 from training_pipeline import train
 
 PROJECT_NAME = 'Graph Bundle Bind Encoder'
-MAIN_EXPERIMENT_NAME = 'gbubi_encoder_magnitude_norm'
+MAIN_EXPERIMENT_NAME = 'gbubi_encoder_magnitude_norm_nn'
 NUM_RUNS = 1
 OTHER_HPARAMS = { #Just the default params that may be different than the ones in the training file
     'wandb_project_name' : PROJECT_NAME,
