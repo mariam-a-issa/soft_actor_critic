0b9f23291bf3c1160382f01278604457e80fec61
Ian Bryant <iwbryant@uci.edu>
2025-09-30 11:32:47 -0700

    Make sure to differentiate between subnet features or not

--- training_pipeline/helpers.py
+++ training_pipeline/helpers.py
@@ -20,12 +20,15 @@ def clean_state(s : NDArray | tuple, graph : bool, env : gym.Env) -> Tensor | Da
         feats = s[0]
     else:
         feats = s
+
     mask = np.ones(feats.shape[1], dtype=bool)
-    mask[1 : address_size + 1] = False  # exclude columns 1 through n
+
     if graph:
+        mask[1 : address_size + 1] = False # exclude columns 1 through n to include subnet feature
         return Data(tensor(s[0][:, mask], dtype=float32), tensor(s[1], dtype=int64)) #0 is node feats and 1 is edge_index. Need to have the data types so that they match up with the rest of the model
-    #return tensor(s[:-1])
-    return tensor(s[:-1, mask])
+    else:
+        mask[: address_size] = False  # exclude columns 0 through n - 1
+        return tensor(s[:-1, mask])
 
 
 def get_action(state : NDArray, env : gym.Env, agent : Agent, graph : bool, explore_steps : int, steps : int) -> tuple[tuple[tuple[int, int], int], Tensor]:
